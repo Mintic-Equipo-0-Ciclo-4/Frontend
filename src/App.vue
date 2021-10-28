cf14795f0a9500c85922bdb1793dfbbf6ac85538
T-hash06 <tomasfelipe.p@gmail.com>
2021-10-28 14:28:42 -0500

    [CREATE] Estructura inicial del proyecto

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+	<div id="main-div">
+		<router-view />
+	</div>
+</template>
+
+<style>
+* {
+	padding: 0px;
+	margin: 0px;
+}
+
+#app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+}
+
+#main-div {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+
+	width: 100vw;
+	height: 100vh;
+}
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,26 @@
+<template>
+	<div id="main-container">
+		<h1>Hola Mundo</h1>
+	</div>
+</template>
+
+<style scoped>
+#main-container {
+	display: grid;
+	grid-template-rows: 80px auto 120px;
+
+	width: 100%;
+	max-width: 800px;
+	height: 100%;
+
+	background-color: red;
+}
+
+@media screen and (min-width: 800px) {
+	#main-container {
+		height: 800px;
+
+		background-color: blue;
+	}
+}
+</style>
