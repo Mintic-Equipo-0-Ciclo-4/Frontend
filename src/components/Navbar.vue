18974156273ee586a7a30faec4b20ce6587eecaf
T-hash06 <tomasfelipe.p@gmail.com>
2021-10-31 12:33:44 -0500

    [UPDATE] Navbar abstraido a un componente Vue

---
+++ src/components/Navbar.vue
@@ -0,0 +1,92 @@
+<template>
+	<div id="navbar-div" :class="{ active }">
+		<router-link class="navbar-link" to="/products">
+			<img src="@/assets/logo.png" alt="" />
+			<h1>Products</h1>
+		</router-link>
+		<router-link class="navbar-link" to="/clients">
+			<img src="@/assets/logo.png" alt="" />
+			<h1>Clients</h1>
+		</router-link>
+		<router-link class="navbar-link" to="/providers">
+			<img src="@/assets/logo.png" alt="" />
+			<h1>Providers</h1>
+		</router-link>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {};
+	},
+	props: ["active"],
+};
+</script>
+
+<style scoped>
+#navbar-div {
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: center;
+	align-content: flex-start;
+
+	position: absolute;
+	top: 80px;
+	left: -400px;
+
+	width: 100%;
+	height: calc(100% - 80px);
+
+	background-color: #fff;
+
+	transition-duration: 600ms;
+	z-index: 1;
+}
+
+#navbar-div.active {
+	left: 0px;
+}
+.navbar-link {
+	display: grid;
+	grid-template-columns: 80px auto;
+
+	width: 95%;
+	height: 60px;
+
+	font-size: 12px;
+	font-family: Roboto-Bold;
+	text-decoration: none;
+	text-align: left;
+	color: #363640;
+
+	margin-top: 30px;
+	border-radius: 8px;
+
+	transition-duration: 200ms;
+}
+
+.navbar-link img {
+	justify-self: center;
+	align-self: center;
+	width: 35px;
+}
+.navbar-link h1 {
+	justify-self: left;
+	align-self: center;
+	margin-left: 8px;
+}
+
+@media screen and (min-width: 813px) {
+	#navbar-div {
+		width: 400px;
+		box-shadow: 5px 0px 5px #dadada;
+		background-color: #fff;
+	}
+}
+
+.router-link-active {
+	color: #6600eb;
+	background-color: #ece0fd;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,11 +4,7 @@
 			<button id="navbar-toggler" @click="navbarActive = !navbarActive"></button>
 			<h1 id="navbar-title">{{ $router.currentRoute._value.name }}</h1>
 		</div>
-		<div id="navbar-div" :class="{ active: navbarActive }">
-			<router-link class="navbar-link" to="/products" @click="navbarActive = false">Productos</router-link>
-			<router-link class="navbar-link" to="/clients" @click="navbarActive = false">Clientes</router-link>
-			<router-link class="navbar-link" to="/providers" @click="navbarActive = false">Proveedores</router-link>
-		</div>
+		<Navbar :active="navbarActive"></Navbar>
 		<div id="view-div">
 			<router-view></router-view>
 		</div>
@@ -16,12 +12,14 @@
 </template>
 
 <script>
+import Navbar from "../components/Navbar.vue";
 export default {
 	data() {
 		return {
 			navbarActive: false,
 		};
 	},
+	components: { Navbar },
 };
 </script>
 
@@ -46,7 +44,7 @@ export default {
 
 #navbar-title {
 	color: #fff;
-	margin-left: 20px;
+	margin-left: 40px;
 }
 #navbar-toggler {
 	width: 50px;
@@ -60,54 +58,11 @@ export default {
 	cursor: pointer;
 }
 
-#navbar-div {
-	display: grid;
-	grid-template-rows: 70px 70px 70px auto;
-
-	position: absolute;
-	top: -100%;
-
-	width: 100%;
-	height: calc(100% - 80px);
-
-	background-color: #363640;
-
-	transition-duration: 600ms;
-	z-index: 1;
-}
-#navbar-div.active {
-	top: 80px;
-}
-#navbar-div a {
-	width: 80%;
-}
-.navbar-link {
-	justify-self: left;
-	align-self: flex-end;
-
-	font-size: 28px;
-	font-family: Roboto-Bold;
-	text-decoration: none;
-	text-align: left;
-	color: #ffffff;
-
-	width: 1000px;
-
-	/* margin-top: 40px; */
-	margin-left: 50px;
-}
-
 #view-div {
 	width: 100%;
 	max-width: 100%;
 	max-height: 100%;
 
 	overflow: auto;
-}
-
-@media screen and (min-width: 813px) {
-	#navbar-div {
-		width: 500px;
-	}
 }
 </style>
