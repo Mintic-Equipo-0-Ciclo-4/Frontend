f43982846bb0b2100db28e148220615ef2fd11ee
T-hash06 <tomasfelipe.p@gmail.com>
2021-11-03 10:43:21 -0500

    [CREATE] Estructura inicial DropMenu

---
+++ src/components/DropMenu.vue
@@ -0,0 +1,12 @@
+<template>
+	<router-link v-for="item of items" :key="item.id" class="navbar-link" :to="`/${item.route}`">
+		<h1>{{ item.name }}</h1>
+	</router-link>
+</template>
+
+<script>
+export default {
+	setup() {},
+	props: ["items"],
+};
+</script>
